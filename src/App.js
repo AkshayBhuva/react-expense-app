4b1d5e02f13041c3a519bdbd240dd846ca03a73b
Akshay Bhuva <akshay.bhuva15@gmail.com>
2022-07-02 18:25:50 +1200

    create ExpenseDate.js and ExpenseDate.css

--- src/App.js
+++ src/App.js
@@ -31,8 +31,8 @@ function App() {
    return (
       <div>
          <h1>Let's get started!</h1>
-         <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-         <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+         <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+         <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
       </div>
    );
 }
